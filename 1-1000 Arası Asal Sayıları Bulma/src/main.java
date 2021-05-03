a523541fe6a9d35aae8b087e1a82eb299dc62652
Yusuf <yusuffil50@gmail.com>
2021-05-03 10:09:25 +0300

    Asal Sayilari Bulma Ornegi

---
+++ 1-1000 Arası Asal Sayıları Bulma/src/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class main {
+
+    public static boolean asalmi(int sayi) {
+        
+        for(int i = 2 ; i < sayi ; i++){
+ 
+        if(sayi%i==0){
+            return false;
+        }
+        
+    }
+        return true;
+    }
+
+    public static void main(String[] args) {
+     for(int i = 2 ; i<=1000 ; i++){
+         if(asalmi(i)){
+             System.out.println(" "+ i);
+         }
+     }
+     
+     
+    }
+    
+}
