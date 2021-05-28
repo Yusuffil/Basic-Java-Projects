69319c1fb4498bde3b02b0d5ee643c8a4eca7932
Yusuf <yusuffil50@gmail.com>
2021-05-28 23:08:55 +0300

    Spring Boot Ornegi

---
+++ northwind/src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,23 @@
+package kodlama.io.northwind.api.controllers;
+
+import kodlama.io.northwind.business.abstracts.ProductService;
+import kodlama.io.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+    @Autowired
+    private ProductService productService;
+
+
+    @GetMapping("/getAll")
+    public List<Product> getAll(){
+    return this.productService.getAll();
+    }
+}
