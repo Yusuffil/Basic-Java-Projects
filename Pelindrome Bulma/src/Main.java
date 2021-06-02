6e285046f09b1ecb6f77b27b6b1cf301d8cdad4b
Yusuf <yusuffil50@gmail.com>
2021-06-02 15:37:23 +0300

    Pelindrome Sayisi Ornegi

---
+++ Pelindrome Bulma/src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cumlenizi Yaziniz.. : ");
+        String cumle = scanner.nextLine();
+        
+        
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i = 0 ; i< cumle.length() / 2 ; i++){
+            stack.push(cumle.charAt(i));
+            if(checkPelindrome(cumle, stack)){
+                System.out.println("Cumlenir Pelindrome cumledir.");
+            }else{
+                System.out.println("Cumle Polindrome degildir...");
+            }
+            
+        }
+    }
+    
+    public static boolean checkPelindrome(String cumle, Stack<Character> stack){
+        for(int i = cumle.length()/2 + 1; i<= cumle.length(); i++){
+            if(cumle.charAt(i) != stack.pop()){
+                return false;
+            }
+        }
+        return false;
+        
+    }
+    }
